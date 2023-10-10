a0b9fdc071129c1aa19a97cff5784374d5176d40
Alex-v-dev <yampi.ru@gmail.com>
2023-10-10 12:42:23 +0500

    init speech.synthesis

--- pages/api/speech/synthesis.ts
+++ pages/api/speech/synthesis.ts
@@ -1,7 +1,13 @@
 import { request } from "undici";
 import { v4 } from "uuid";
 import type { NextApiRequest, NextApiResponse } from "next";
+import * as fs from "fs";
+import path from "path";
+import { AUDIO_FOLDER } from "@/src/constants/paths";
+const Lame = require("node-lame").Lame;
+
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
 class Speech {
   private accessToken = "";
   private expiresAt = Date.now();
@@ -20,7 +26,7 @@ class Speech {
           "process.env.SBER_SALUTE_SPEECH_TOKEN"
         );
         try {
-          const { statusCode, headers, trailers, body } = await request(
+          const { statusCode, headers, body } = await request(
             "https://ngw.devices.sberbank.ru:9443/api/v2/oauth",
             {
               method: "POST",
@@ -60,12 +66,51 @@ class Speech {
 
     return this._authRequest;
   }
+
+  async synthesis(): Promise<string> {
+    await this.auth();
+
+    console.log(this.accessToken, "this.accessToken");
+    const { statusCode, headers, body } = await request(
+      "https://smartspeech.sber.ru/rest/v1/text:synthesize?format=wav16&voice=Nec_24000",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+          "Content-Type": "application/ssml",
+        },
+        body: "Привет",
+      }
+    );
+    console.log(statusCode, "statusCode");
+    const arrayBuffer = await body.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const mp3FilePath = path.join(AUDIO_FOLDER, `${v4()}.mp3`);
+
+    await new Promise<void>((resolve, reject) => {
+      const encoder = new Lame({
+        output: mp3FilePath,
+        bitrate: 192,
+      }).setBuffer(buffer);
+
+      encoder
+        .encode()
+        .then(() => {
+          resolve();
+        })
+        .catch((error: any) => {
+          reject(error);
+        });
+    });
+
+    return mp3FilePath.replaceAll(path.sep, "/");
+  }
 }
 
 export const speech = new Speech();
 
 type ResponseData = {
-  message: string;
+  filePath: string;
 };
 
 export default async function handler(
@@ -73,6 +118,7 @@ export default async function handler(
   res: NextApiResponse<ResponseData>
 ) {
   console.log("1", 1);
-  await speech.auth();
-  res.status(200).json({ message: "Hello from Next.js!" });
+  const filePath = await speech.synthesis();
+
+  res.status(200).json({ filePath });
 }
--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -2,6 +2,8 @@ export const VIDEO_FOLDER_ABSOLUTE_PATH = "C:\\code\\neural-video\\temp\\video";
 
 export const IMG_FOLDER = "C:\\code\\neural-video\\temp\\img";
 
+export const AUDIO_FOLDER = "C:\\code\\neural-video\\temp\\audio";
+
 export const TRRRRR_ANIMATION_MP3 =
   "C:\\code\\neural-video\\public\\audio\\trrrrr.mp3";
 export const GAME_STOP_ANIMATION_MP3 =
