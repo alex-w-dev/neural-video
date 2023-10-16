fb4fab384d60026bb1e459041dfa704e29c2d36c
Alex-v-dev <yampi.ru@gmail.com>
2023-10-16 22:02:42 +0500

    add neural add to video

--- src/film/animations/scientist-answer.animation.ts
+++ src/film/animations/scientist-answer.animation.ts
@@ -1,6 +1,6 @@
 import { FilmAnimation } from "./film-animation";
 import { Film } from "@/src/film/film.class";
-import { Text, TextStyle } from "pixi.js";
+import { Sprite, Text, TextStyle } from "pixi.js";
 import { tween } from "@/src/utils/tween";
 // @ts-ignore
 import * as TWEEN from "@tweenjs/tween.js/dist/tween.esm.js";
@@ -15,11 +15,66 @@ export class ScientistAnswerAnimation extends FilmAnimation {
     // this.film.app.stage.addChild();
   }
 
+  playGptAddAnimation() {
+    const y = 0.5;
+    const sprite = Sprite.from(
+      "https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg"
+    );
+    sprite.anchor.x = 0.5;
+    sprite.anchor.y = 0.5;
+    sprite.x = 1080 * 0.7;
+    sprite.y = 1920 * (0.15 + y);
+    sprite.scale.set(0.8);
+    sprite.renderable = false;
+    this.film.app.stage.addChild(sprite);
+
+    const neuralText = this.getSubtitleText("нейросеть", {
+      fontSize: 55,
+      fill: "#75ac9d",
+    });
+    neuralText.anchor.x = 0.5;
+    neuralText.anchor.y = 0.5;
+    neuralText.x = 1080 * 0.7;
+    neuralText.y = 1920 * (0.2 + y);
+    neuralText.renderable = false;
+    this.film.app.stage.addChild(neuralText);
+
+    const inCommentsText = this.getSubtitleText("в комментариях", {
+      fontSize: 55,
+      fill: "#75ac9d",
+    });
+    inCommentsText.anchor.x = 0.5;
+    inCommentsText.anchor.y = 0.5;
+    inCommentsText.x = 1080 * 0.7;
+    inCommentsText.y = 1920 * (0.2 + y);
+    inCommentsText.renderable = false;
+    this.film.app.stage.addChild(inCommentsText);
+
+    let i = 0;
+    const interval = setInterval(() => {
+      if (!this.isPlaying) {
+        clearInterval(interval);
+      }
+      if (i < 2 || i > 5 || !this.isPlaying) {
+        neuralText.renderable = false;
+        inCommentsText.renderable = false;
+        sprite.renderable = false;
+      } else {
+        const odd = i % 2;
+        neuralText.renderable = !odd;
+        inCommentsText.renderable = !!odd;
+        sprite.renderable = true;
+      }
+      i++;
+    }, 500);
+  }
+
   play() {
     this.isPlaying = true;
     const start = Date.now();
     let lastRenderedImageIndex = 0;
 
+    this.playGptAddAnimation();
     const a = () => {
       const now = Date.now();
       const currentTime = now - start;
@@ -73,5 +128,26 @@ export class ScientistAnswerAnimation extends FilmAnimation {
 
   needImagesLength(): number {
     return 0;
+  }
+
+  private getSubtitleText(text: string, textStyle: Partial<TextStyle>): Text {
+    const style = new TextStyle({
+      align: "center",
+      dropShadow: true,
+      dropShadowAlpha: 0.6,
+      dropShadowAngle: -2.2,
+      dropShadowBlur: 8,
+      dropShadowDistance: 0,
+      fill: "#feaa34",
+      fontFamily: "Comic Sans MS",
+      fontSize: 70,
+      fontWeight: "bold",
+      lineJoin: "round",
+      miterLimit: 11,
+      strokeThickness: 7,
+      wordWrap: false,
+      ...textStyle,
+    });
+    return new Text(text, style);
   }
 }
